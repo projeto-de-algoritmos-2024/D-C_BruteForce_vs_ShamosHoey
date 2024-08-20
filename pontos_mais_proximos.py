5030c928d914b363b44bd4b5a7387dbb29c26762
Mateus Villela Consorte <108163301+MVConsorte@users.noreply.github.com>
2024-08-19 23:12:21 -0300

    Update pontos_mais_proximos.py

--- pontos_mais_proximos.py
+++ pontos_mais_proximos.py
@@ -7,217 +7,248 @@ from pygame.sprite import Sprite
 
 pg.init()
 
-def dividir_pontos(pontos):
-    p = deque(pontos)
-    esquerda = [p.popleft() for _ in range(len(p)//2)]
-    direita = [p.popleft() for _ in range(len(p))]
-    return esquerda, direita
+def split_points(points):
+    """Divide a lista de pontos em duas partes aproximadamente iguais."""
+    p = deque(points)
+    left = [p.popleft() for _ in range(len(p)//2)]
+    right = [p.popleft() for _ in range(len(p))]
+    return left, right
 
-def forca_bruta(circulos):
-    if len(circulos) <= 1:
+def brute_force_pair(circles):
+    """Encontra o par mais próximo usando o método de força bruta."""
+    if len(circles) <= 1:
         return []
-    if len(circulos) == 2:
-        return circulos
+    if len(circles) == 2:
+        return circles
 
-    par = [circulos[0], circulos[1]]
-    menor_distancia = distancia(par[0], par[1])
-    for i in range(len(circulos)):
-        for j in range(i + 1, len(circulos)):
-            nova_distancia = distancia(circulos[i], circulos[j])
-            if nova_distancia < menor_distancia:
-                par = [circulos[i], circulos[j]]
-                menor_distancia = nova_distancia
+    pair = [circles[0], circles[1]]
+    shortest_dist = distance(pair[0], pair[1])
+    for i in range(len(circles)):
+        for j in range(len(circles)):
+            if i == j: continue
+            new_dist = distance(circles[i], circles[j])
+            if new_dist < shortest_dist:
+                pair = [circles[i], circles[j]]
+                shortest_dist = new_dist
 
-    return par
+    return pair
 
-def mais_proximo_na_faixa(circulos):
-    if len(circulos) < 2:
+def closest_within_band(circles):
+    """Encontra o par mais próximo dentro de uma faixa de largura fixa."""
+    if len(circles) < 2:
         return None
-    par = [circulos[0], circulos[1]]
-    menor_distancia = distancia(par[0], par[1])
-    circulos = sorted(circulos, key=lambda s: s.pos.y)
-    for i in range(len(circulos)):
-        for j in range(i+1, min(len(circulos), i+7)):
-            nova_distancia = distancia(circulos[i], circulos[j])
-            if nova_distancia < menor_distancia:
-                par = [circulos[i], circulos[j]]
-                menor_distancia = nova_distancia
+    pair = [circles[0], circles[1]]
+    shortest_dist = distance(pair[0], pair[1])
+    circles = sorted(circles, key=lambda s: s.pos.y)
+    for i in range(len(circles)):
+        for j in range(i+1, min(len(circles), i+7)):
+            new_dist = distance(circles[i], circles[j])
+            if new_dist < shortest_dist:
+                pair = [circles[i], circles[j]]
+                shortest_dist = new_dist
 
-    return par
+    return pair
 
-def dividir_e_conquistar(circulos):
-    if len(circulos) <= 3:
-        return forca_bruta(circulos)
+def divide_and_conquer(circles):
+    """Encontra o par mais próximo usando o algoritmo de divisão e conquista."""
+    if len(circles) <= 3:
+        return brute_force_pair(circles)
 
-    esquerda, direita = dividir_pontos(circulos)
-    par_esquerda_mais_proximo = dividir_e_conquistar(esquerda)
-    par_direita_mais_proximo = dividir_e_conquistar(direita)
+    left, right = split_points(circles)
+    closest_left_pair = divide_and_conquer(left)
+    closest_right_pair = divide_and_conquer(right)
 
-    distancia_esquerda = distancia(par_esquerda_mais_proximo[0], par_esquerda_mais_proximo[1])
-    distancia_direita = distancia(par_direita_mais_proximo[0], par_direita_mais_proximo[1])
-    delta = min(distancia_esquerda, distancia_direita)
+    distance_left = distance(closest_left_pair[0], closest_left_pair[1])
+    distance_right = distance(closest_right_pair[0], closest_right_pair[1])
+    delta = min(distance_left, distance_right)
 
-    meio = esquerda[len(esquerda)-1].pos.x
-    circulos_na_faixa = [a for a in circulos if abs(a.pos.x - meio) < delta]
-    par_mais_proximo_na_faixa = mais_proximo_na_faixa(circulos_na_faixa)
+    middle = left[len(left)-1].pos.x
+    circles_within_band = [a for a in circles if abs(a.pos.x - middle) < delta]
+    closest_pair_band = closest_within_band(circles_within_band)
 
-    if par_mais_proximo_na_faixa:
-        if distancia(par_mais_proximo_na_faixa[0], par_mais_proximo_na_faixa[1]) < delta:
-            return par_mais_proximo_na_faixa
+    if closest_pair_band:
+        if distance(closest_pair_band[0], closest_pair_band[1]) < delta:
+            return closest_pair_band
 
-    return par_esquerda_mais_proximo if distancia_esquerda < distancia_direita else par_direita_mais_proximo
+    return closest_left_pair if distance_left < distance_right else closest_right_pair
 
-def ajustar_direcao_aleatoriamente() -> float:
+def adjust_direction_randomly() -> float:
+    """Ajusta a direção aleatoriamente."""
     return (random.random() - 0.5) * 0.2
 
-def verificar_colisao_com_parede(posicao, direcao) -> float:
-    x, y = posicao
-    if x > 750: direcao = np.pi
-    if x < 0: direcao = 0
-    if y > 750: direcao = np.pi/2
-    if y < 0: direcao = np.pi*1.5
+def wall_collision_check(pos, direction) -> float:
+    """Verifica e ajusta a direção em caso de colisão com as bordas da tela."""
+    x, y = pos
+    if x > 750: direction = np.pi
+    if x < 0: direction = 0
+    if y > 750: direction = np.pi/2
+    if y < 0: direction = np.pi*1.5
 
-    return direcao
+    return direction
 
-def distancia(circulo1, circulo2) -> float:
+def distance(circle1, circle2) -> float:
+    """Calcula a distância entre dois círculos."""
     return np.sqrt(
-        (np.abs(circulo1.pos.x - circulo2.pos.x)**2) +
-        (np.abs(circulo1.pos.y - circulo2.pos.y)**2)
+        (np.abs(circle1.pos.x - circle2.pos.x)**2) + 
+        (np.abs(circle1.pos.y - circle2.pos.y)**2)
     )
 
-class Ponto:
+class Point:
+    """Representa um ponto no plano 2D."""
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Circulo(Sprite):
-    def __init__(self, jogo, x, y):
-        Sprite.__init__(self, jogo.circulos)
-        self.jogo = jogo
-        self.raio = 5
-        self.image = pg.Surface((self.raio * 2, self.raio * 2), pg.SRCALPHA)
-        pg.draw.circle(self.image, (0, 0, 0), (self.raio, self.raio), self.raio)
+class Circle(Sprite):
+    """Representa um círculo no jogo."""
+    def __init__(self, game, x, y):
+        Sprite.__init__(self, game.circles)
+        self.game = game
+        self.radius = 5  # Tamanho do círculo
+        self.image = pg.Surface((self.radius * 2, self.radius * 2), pg.SRCALPHA)
+        pg.draw.circle(self.image, (0, 0, 0), (self.radius, self.radius), self.radius)
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.pos = Ponto(x, y)
-        self.direcao = random.random() * 2 * np.pi
-        self.velocidade = 3
+        self.pos = Point(x, y)
+        self.direction = random.random() * 2 * np.pi
+        self.speed = 3
 
-    def caminhada_aleatoria(self):
-        self.direcao = verificar_colisao_com_parede(self.rect.center, self.direcao)
-        self.direcao += ajustar_direcao_aleatoriamente()
+    def random_walk(self):
+        """Move o círculo aleatoriamente, ajustando a direção se necessário."""
+        self.direction = wall_collision_check(self.rect.center, self.direction)
+        self.direction += adjust_direction_randomly()
 
-        mover_x = np.cos(self.direcao) * self.velocidade
-        mover_y = np.sin(self.direcao) * -self.velocidade
-        self.pos.x += mover_x
-        self.pos.y += mover_y
+        move_x = np.cos(self.direction) * self.speed
+        move_y = np.sin(self.direction) * -self.speed
+        self.pos.x += move_x
+        self.pos.y += move_y
         self.rect.centerx = self.pos.x
         self.rect.centery = self.pos.y
 
     def update(self):
-        self.caminhada_aleatoria()
+        """Atualiza o estado do círculo."""
+        self.random_walk()
 
-class Jogo:
+class Game:
+    """Representa o jogo de comparação de algoritmos de pares mais próximos."""
     def __init__(self):
-        pg.display.set_caption('Par Mais Próximo - Comparar Algoritmos')
-        self.relogio = pg.time.Clock()
-        self.tela = pg.display.set_mode((1500, 750))
-        self.fonte = pg.font.Font(None, 36)
-        self.intervalo_adicionar_circulo = 1000
-        self.ultimo_tempo_adicionar_circulo = pg.time.get_ticks()
-        self.circulos = pg.sprite.Group()
-        self.algoritmo = dividir_e_conquistar
-        self.nome_algoritmo = "Dividir e Conquistar"
+        pg.display.set_caption('Closest Pair - Compare Algorithms')
+        self.clock = pg.time.Clock()
+        self.screen = pg.display.set_mode((1500, 750))  # Tela dividida
+        self.font = pg.font.Font(None, 36)
+        self.add_circle_interval = 1000  # Intervalo em milissegundos
+        self.last_circle_add_time = pg.time.get_ticks()
+        self.circles = pg.sprite.Group()
+        self.algorithm = divide_and_conquer  # Algoritmo padrão
+        self.algorithm_name = "Divide e Conquista"
 
-    def novo(self):
-        self.circulos.empty()
-        for _ in range(50):
-            self.adicionar_circulo()
+    def new(self):
+        """Inicializa o jogo com círculos novos."""
+        self.circles.empty()
+        for _ in range(50):  # define o número de círculos iniciais
+            self.add_circle()
 
-    def rodar(self):
-        self.jogando = True
-        while self.jogando:
-            self.relogio.tick(60)
-            self.eventos()
-            self.atualizar()
-            self.desenhar()
+    def run(self):
+        """Inicia o loop principal do jogo."""
+        self.playing = True
+        while self.playing:
+            self.clock.tick(60)
+            self.events()
+            self.update()
+            self.draw()
 
-    def desenhar(self):
-        self.tela.fill((255, 255, 255))
+    def draw(self):
+        """Desenha os círculos e a interface gráfica do jogo."""
+        # Limpa a tela principal
+        self.screen.fill((255, 255, 255))
 
-        superficie_esquerda = pg.Surface((750, 750), pg.SRCALPHA)
-        superficie_direita = pg.Surface((750, 750), pg.SRCALPHA)
+        # Cria superfícies para cada lado da tela
+        left_surface = pg.Surface((750, 750), pg.SRCALPHA)
+        right_surface = pg.Surface((750, 750), pg.SRCALPHA)
 
-        superficie_esquerda.fill((255, 255, 255))
-        superficie_direita.fill((255, 255, 255))
+        # Limpa as superfícies
+        left_surface.fill((255, 255, 255))
+        right_surface.fill((255, 255, 255))
 
-        if self.nome_algoritmo == "Dividir e Conquistar":
-            par_dc = dividir_e_conquistar(list(self.circulos))
-            if par_dc:
-                for circulo in self.circulos:
-                    if circulo in par_dc:
-                        pg.draw.circle(circulo.image, (200, 0, 0), (circulo.raio, circulo.raio), circulo.raio)
+        # Algoritmo Divide e Conquista
+        if self.algorithm_name == "Divide e Conquista":
+            pair_dc = divide_and_conquer(list(self.circles))
+            if pair_dc:
+                for circle in self.circles:
+                    if circle in pair_dc:
+                        pg.draw.circle(circle.image, (200, 0, 0), (circle.radius, circle.radius), circle.radius)
                     else:
-                        pg.draw.circle(circulo.image, (0, 0, 0), (circulo.raio, circulo.raio), circulo.raio)
-            self.circulos.draw(superficie_esquerda)
-            self.exibir_informacoes(superficie_esquerda, len(self.circulos), "Dividir e Conquistar")
+                        pg.draw.circle(circle.image, (0, 0, 0), (circle.radius, circle.radius), circle.radius)
+            self.circles.draw(left_surface)
+            self.display_info(left_surface, len(self.circles), "Divide e Conquista")
 
-        elif self.nome_algoritmo == "Força Bruta":
-            par_fb = forca_bruta(list(self.circulos))
-            if par_fb:
-                for circulo in self.circulos:
-                    if circulo in par_fb:
-                        pg.draw.circle(circulo.image, (200, 0, 0), (circulo.raio, circulo.raio), circulo.raio)
+        # Algoritmo Força Bruta
+        elif self.algorithm_name == "Força Bruta":
+            pair_bf = brute_force_pair(list(self.circles))
+            if pair_bf:
+                for circle in self.circles:
+                    if circle in pair_bf:
+                        pg.draw.circle(circle.image, (200, 0, 0), (circle.radius, circle.radius), circle.radius)
                     else:
-                        pg.draw.circle(circulo.image, (0, 0, 0), (circulo.raio, circulo.raio), circulo.raio)
-            self.circulos.draw(superficie_direita)
-            self.exibir_informacoes(superficie_direita, len(self.circulos), "Força Bruta")
+                        pg.draw.circle(circle.image, (0, 0, 0), (circle.radius, circle.radius), circle.radius)
+            self.circles.draw(right_surface)
+            self.display_info(right_surface, len(self.circles), "Força Bruta")
 
-        self.tela.blit(superficie_esquerda, (0, 0))
-        self.tela.blit(superficie_direita, (750, 0))
-        pg.display.flip()
+        # Atualiza a tela principal com as superfícies divididas
+        self.screen.blit(left_surface, (0, 0))
+        self.screen.blit(right_surface, (750, 0))
+        pg.display.update()
 
-    def exibir_informacoes(self, superficie, num_circulos, algoritmo):
-        texto = self.fonte.render(f'Algoritmo: {algoritmo}', True, (0, 0, 0))
-        superficie.blit(texto, (10, 10))
-        texto = self.fonte.render(f'Círculos: {num_circulos}', True, (0, 0, 0))
-        superficie.blit(texto, (10, 50))
+    def update(self):
+        """Atualiza o estado do jogo e adiciona novos círculos conforme necessário."""
+        self.circles.update()
 
-    def adicionar_circulo(self):
-        x = random.randint(0, 750)
-        y = random.randint(0, 750)
-        Circulo(self, x, y)
+        # Verifica se é hora de adicionar um novo círculo
+        current_time = pg.time.get_ticks()
+        if current_time - self.last_circle_add_time > self.add_circle_interval:
+            self.add_circle()
+            self.last_circle_add_time = current_time
 
-    def atualizar(self):
-        self.circulos.update()
+    def events(self):
+        """Processa eventos de entrada do usuário."""
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.toggle_algorithm()
 
-        tempo_atual = pg.time.get_ticks()
-        if tempo_atual - self.ultimo_tempo_adicionar_circulo > self.intervalo_adicionar_circulo:
-            self.adicionar_circulo()
-            self.ultimo_tempo_adicionar_circulo = tempo_atual
+    def display_info(self, sub_screen, n_points, algorithm_name):
+        """Exibe informações sobre o algoritmo e o FPS."""
+        # Calcular o FPS
+        fps = self.clock.get_fps()
+        # Texto para exibir o número de pontos e FPS
+        text = f'{algorithm_name}: Pontos: {n_points} - FPS: {fps:.2f}'
+        # Renderizar o texto
+        text_surface = self.font.render(text, True, (0, 0, 0))
+        # Desenhar o texto na tela
+        sub_screen.blit(text_surface, (10, 10))
 
-    def eventos(self):
-        for evento in pg.event.get():
-            if evento.type == pg.QUIT:
-                self.jogando = False
-                pg.quit()
-                sys.exit()
+    def add_circle(self):
+        """Adiciona um novo círculo na tela."""
+        x = random.randint(10, 740)
+        y = random.randint(10, 740)
+        Circle(self, x, y)
 
-            if evento.type == pg.KEYDOWN:
-                if evento.key == pg.K_q:
-                    self.jogando = False
-                    pg.quit()
-                    sys.exit()
+    def toggle_algorithm(self):
+        """Alterna entre os algoritmos de comparação de pares mais próximos."""
+        if self.algorithm_name == "Divide e Conquista":
+            self.algorithm_name = "Força Bruta"
+            self.algorithm = brute_force_pair
+        else:
+            self.algorithm_name = "Divide e Conquista"
+            self.algorithm = divide_and_conquer
 
-                if evento.key == pg.K_SPACE:
-                    if self.nome_algoritmo == "Dividir e Conquistar":
-                        self.nome_algoritmo = "Força Bruta"
-                        self.algoritmo = forca_bruta
-                    else:
-                        self.nome_algoritmo = "Dividir e Conquistar"
-                        self.algoritmo = dividir_e_conquistar
+    def quit(self):
+        """Encerra o jogo e fecha o pygame."""
+        pg.quit()
+        sys.exit()
 
-if __name__ == '__main__':
-    jogo = Jogo()
-    jogo.novo()
-    jogo.rodar()
+g = Game()
+g.new()
+g.run()
